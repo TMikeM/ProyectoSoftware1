1e1ca868d2a73571570307968696529ba4b66a57
skipword <orduzcarvajal.cristian2017@gmail.com>
2024-12-03 22:05:38 -0500

    .

---
+++ lunchuis/src/main/java/com/project/lunchuis/Model/User.java
@@ -0,0 +1,25 @@
+package com.project.lunchuis.Model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String password;
+    private String email;
+    private String dni;
+    private String phoneNumber;
+    private Rol rol;
+    private Boolean session;
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Buy> purchases;
+}
