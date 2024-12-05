da613e9191a09afe250d24d100ce305dc4e6d1b0
DAlejandroGB <dagb170422@gmail.com>
2024-12-05 00:52:37 -0500

    Actualizacion Bootstrap

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LobbyComponent } from './lobby/lobby.component';
+import { AdministrativeComponent } from './administrative/administrative.component';
 
 const routes: Routes = [
-  { path:'', redirectTo:'/login', pathMatch:'full'},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'lobby', component: LobbyComponent}
+  { path: 'lobby', component: LobbyComponent },
+  { path: 'admin', component: AdministrativeComponent }
 ];
 
 @NgModule({
